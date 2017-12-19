215c7176a8420869a77e56ef90dab7c56c7883a7
Roxana Gogonea <roxana@MacBook-Air.local>
2017-12-19 13:07:32 +0100

    Obtain CVE list related to camera vulnerabilities

--- datadrivensecurity.R
+++ datadrivensecurity.R
@@ -1,20 +1,50 @@
-#Install shodan packate, uncomment if needed
+## Needed packages installation
+#install.packages("devtools")
+#install.packages("tidyr")
+#install.packages("dplyr")
+#install.packages("stringr")
+#install.packages("foreach")
+
+##Shodan package expects SHODAN_API_KEY to be defined
 #devtools::install_github("hrbrmstr/shodan")
-#Shodan package expects SHODAN_API_KEY to be defined
 
-#Load shodan library
+##Load packages
 library(shodan)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(foreach)
 
-#Download sysdata file that contains CVEs and CPEs information
-download.file(url = "https://github.com/r-net-tools/security.datasets/raw/master/net.security/sysdata.rda", "sysdata.rda")
+##Download sysdata file that contains CVEs and CPEs information
+download.file(url = "https://github.com/r-net-tools/security.datasets/raw/master/net.security/sysdata.rda", destfile = "sysdata.rda")
 
-#Load sysdata file
+##Load sysdata file
 load("sysdata.rda")
 
-# Create new lists with CPEs and CVEs information
+##Create new lists with CVEs information
 cpes <- netsec.data$datasets$cpes
 cves <- netsec.data$datasets$cves
 
+##Create vector with camera vendors that will be checked to create the research
+search_cameras <- c("axis.*?camera","axis.*?neteye" , "d-link.*?camera", "mobotix.*?camera")
+cpe_fields <- c("cpe", "type", "vendor", "model", "version")
+cve_selected_fields <- c("cve", "cvss", "description", "cpe.software")
 
+##Get cameras information
+cameras <- data.frame(cves %>% filter(str_detect(cpe.software, paste(search_cameras, collapse = "|"))) %>% select(cve_selected_fields))
+
+##Define a new empty data frame
+cameras_data <- data.frame(cve = character(), cvss = double(), description = character(), type = character(), vendor = character(), model = character(), version = character())
+
+##Get the CPE software information and create a new dataframe with the information that is related to cameras
+for (row in 1:nrow(cameras)) {
+  camera <- cameras[row,]
+  
+  ##Parse cpe.software to JSON and filter its information in order to extract only camera related information
+  ##Once filtered, separate fields in cpe fields: cpe, type, vendor, model and version
+  software_list <- data.frame(software = jsonlite::fromJSON(camera$cpe.software), stringsAsFactors = FALSE) %>% filter(str_detect(software, paste(search_cameras, collapse = "|"))) %>% separate(software, cpe_fields, sep = ":")
+  
+  cameras_data = rbind(cameras_data, data.frame(camera[1:3], software_list[2:5])) 
+}
 
 
